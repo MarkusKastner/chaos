b9bef10eed553afe8f49657dda6b33bf39a97ca0
Markus Kastner <MarkusKastner@Markuss-MacBook-Pro.local>
2016-09-29 22:29:32 +0200

    test

---
+++ CrossPlatform1/CrossPlatform1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrossPlatform1
+//
+//  Created by Markus Kastner on 29/09/2016.
+//  Copyright © 2016 Markus Kastner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
